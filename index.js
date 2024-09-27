5de520c18412f24b503bc8dd4ef8001106354ff6
vipulattri <133794719+vipulattri@users.noreply.github.com>
2024-09-27 20:00:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function gameEngine() {
         gameOverSound.play();
         musicSound.pause();
         inputDir = { x: 0, y: 0 };
-        alert("Pyar me aadmi andha hota hai! Abhishek bhi tha!");
+
         snakeArr = [{ x: 13, y: 15 }];
         musicSound.play();
         score = 0;
